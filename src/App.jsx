685f65150e51e3704ebb8d3429bfc8bb4c686b76
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-08-17 14:42:38 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>
-      Hello!
+    <div className="relative flex flex-col">
+      <Navbar/>
     </div>
   )
 }
