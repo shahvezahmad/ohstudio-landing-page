d5938c9d3bddb0725cb774b41c314b4d4e829651
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-09-03 22:37:20 +0530

    Profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Shop from './Footer/shop';
+import Profile from './Profile/profile';
 
 const App = () => {
   return (
     <div className="relative flex flex-col">
       <Navbar/>
+      <Profile/>
       <Shop/>
     </div>
   )
